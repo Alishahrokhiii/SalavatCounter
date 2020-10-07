22fea192996840e4c7c6d7a8918faf18ce6b6516
Seyed Ali Shahrokhi <Alishahrokhi@icloud.com>
2020-10-07 18:01:43 +0330

    Initial Commit

---
+++ SalavatCounter WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  SalavatCounter WatchKit Extension
+//
+//  Created by Seyed Ali Shahrokhi on 7/16/1399 AP.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ SalavatCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SalavatCounter
+//
+//  Created by Seyed Ali Shahrokhi on 7/16/1399 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
