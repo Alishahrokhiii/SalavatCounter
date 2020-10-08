9bec230ef774176e0227e80cc590f4d50cb3e13b
Seyed Ali Shahrokhi <Alishahrokhi@icloud.com>
2020-10-08 14:11:25 +0330

    WatchOs V1.0.0 & Bug Fixes

---
+++ SalavatCounter/Helpers/Utilities.swift
@@ -0,0 +1,81 @@
+//
+//  Utilities.swift
+//  customauth
+//
+//  Created by Christopher Ching on 2019-05-09.
+//  Copyright © 2019 Christopher Ching. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Utilities {
+    
+    static func styleTextField(_ textfield:UITextField) {
+        
+        // Create the bottom line
+        let bottomLine = CALayer()
+        
+        bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 2)
+        
+        bottomLine.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1).cgColor
+        
+        // Remove border on text field
+        textfield.borderStyle = .none
+        
+        // Add the line to the text field
+        textfield.layer.addSublayer(bottomLine)
+        
+    }
+    
+    static func styleFilledButton(_ button:UIButton) {
+        // Filled rounded corner style
+        button.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
+        button.layer.cornerRadius = 25.0
+        button.tintColor = UIColor.white
+        
+    }
+    static func styleFilledButtonButLessRoundEdges(_ button:UIButton) {
+        
+        // Filled rounded corner style
+        button.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
+        button.layer.cornerRadius = 10.0
+        button.tintColor = UIColor.white
+    }
+    
+    static func styleHollowButton(_ button:UIButton) {
+        
+        // Hollow rounded corner style
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 25.0
+        button.layer.shadowOpacity = 0.25
+        button.layer.shadowRadius = 5
+        button.layer.shadowOffset = CGSize(width: 0, height: 10)
+        button.tintColor = UIColor.black
+    }
+    static func styleHollowButtonNotRound(_ button:UIButton) {
+        
+        // Hollow rounded corner style
+        button.layer.cornerRadius = 5
+        button.layer.shadowOpacity = 0.10
+        button.layer.shadowRadius = 4
+        button.layer.shadowOffset = CGSize(width: 0, height: 10)
+    }
+    static func breakLine(_ View:UIView) {
+        
+        // Hollow rounded corner style
+        View.layer.cornerRadius = 5
+        View.layer.shadowOpacity = 0.10
+        View.layer.shadowRadius = 4
+        View.layer.shadowOffset = CGSize(width: 0, height: 10)
+    }
+    
+    
+    static func isPasswordValid(_ password : String) -> Bool {
+        
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
+}
